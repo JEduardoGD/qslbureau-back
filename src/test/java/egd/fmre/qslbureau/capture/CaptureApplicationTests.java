3f326e1eb412291fe62bbe49d1cf653a64561b55
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2023-02-05 10:30:23 -0600

    first commit

---
+++ src/test/java/egd/fmre/qslbureau/capture/CaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package egd.fmre.qslbureau.capture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
