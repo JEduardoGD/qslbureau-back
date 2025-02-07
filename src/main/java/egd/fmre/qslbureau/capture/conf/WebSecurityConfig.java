ceb0e1e0c5d8a6fee3921040304be17179d46b21
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2025-02-07 08:22:50 -0600

    adding swagger

--- src/main/java/egd/fmre/qslbureau/capture/conf/WebSecurityConfig.java
+++ src/main/java/egd/fmre/qslbureau/capture/conf/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 
 import egd.fmre.qslbureau.capture.component.JwtAuthenticationEntryPoint;
@@ -79,9 +81,12 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
-				.authorizeHttpRequests(
-						c -> c.requestMatchers("/authenticate", "/actuator/**", "/portal/**", "/actuator/**")
-								.permitAll().anyRequest().authenticated())
+				.authorizeHttpRequests(authz -> authz
+						.requestMatchers(new AntPathRequestMatcher("/authenticate/**", HttpMethod.POST.name())).permitAll()
+						.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+						//.requestMatchers("/authenticate", "/actuator/**", "/portal/**", "/actuator/**", "/swagger-ui/**").permitAll()
+						//.requestMatchers("/**").permitAll()
+						.anyRequest().authenticated())
 				// .formLogin(c -> c.loginPage("/login").permitAll())
 				// .logout(LogoutConfigurer::permitAll)
 				.exceptionHandling(r -> r.authenticationEntryPoint(jwtAuthenticationEntryPoint))
